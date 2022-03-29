9fa2559e0b8389bb30d7158f3e8058254d8fd0bc
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-03-29 00:48:15 +0000

    btn suivant done

--- application/app/src/main/java/com/careline/MainActivity.java
+++ application/app/src/main/java/com/careline/MainActivity.java
@@ -2,13 +2,27 @@ package com.careline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button suivant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       suivant=(Button) findViewById(R.id.btn_suivant);
+       suivant.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent=new Intent(MainActivity.this,Start_2.class);
+               startActivity(intent);
+           }
+       });
+
     }
 }
---
+++ application/app/src/main/java/com/careline/Start_3.java
@@ -0,0 +1,14 @@
+package com.careline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Start_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start3);
+    }
+}
