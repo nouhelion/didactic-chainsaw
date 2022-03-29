0592ffbfb4d3e542f79e3f1c6e51745051f77ac8
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-03-29 00:19:39 +0000

    second page done

---
+++ application/app/src/main/java/com/careline/Start_2.java
@@ -0,0 +1,14 @@
+package com.careline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Start_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start2);
+    }
+}
