5034f7837a119b209bee6f54840401cdb09abc0e
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-04-08 21:44:32 +0000

    white background

--- application/app/src/main/java/com/careline/Login.java
+++ application/app/src/main/java/com/careline/Login.java
@@ -2,13 +2,27 @@ package com.careline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
+    TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        register=(TextView) findViewById(R.id.insc);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Login.this,Register.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- application/app/src/main/java/com/careline/Register.java
+++ application/app/src/main/java/com/careline/Register.java
@@ -2,13 +2,27 @@ package com.careline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
+    TextView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        login=(TextView) findViewById(R.id.log);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Register.this,Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
