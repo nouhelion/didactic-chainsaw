62e47c3728edf872ac08f0d645a6bc332ea7d5a4
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-04-08 22:36:00 +0000

    welcome interface

--- application/app/src/main/java/com/careline/Login.java
+++ application/app/src/main/java/com/careline/Login.java
@@ -11,15 +11,24 @@ import android.widget.TextView;
 public class Login extends AppCompatActivity {
 
     TextView register;
+    Button welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         register=(TextView) findViewById(R.id.insc);
+        welcome=(Button) findViewById(R.id.login);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Login.this,Register.class);
+                startActivity(intent);
+            }
+        });
+        welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Login.this,Welcome.class);
                 startActivity(intent);
             }
         });
--- application/app/src/main/java/com/careline/Register.java
+++ application/app/src/main/java/com/careline/Register.java
@@ -11,15 +11,24 @@ import android.widget.TextView;
 public class Register extends AppCompatActivity {
 
     TextView login;
+    Button welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         login=(TextView) findViewById(R.id.log);
+        welcome=(Button) findViewById(R.id.insc);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Register.this,Login.class);
+                startActivity(intent);
+            }
+        });
+        welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Register.this,Welcome.class);
                 startActivity(intent);
             }
         });
