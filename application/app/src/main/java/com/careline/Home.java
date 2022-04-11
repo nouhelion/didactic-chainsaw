eb215ad306faa8090142026106eac45e68aa8ee1
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-04-11 21:21:10 +0000

    meh

--- application/app/src/main/java/com/careline/Home.java
+++ application/app/src/main/java/com/careline/Home.java
@@ -1,8 +1,13 @@
 package com.careline;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +15,9 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        BottomNavigationView navbar  = findViewById(R.id.bottomNavigationView);
+        NavController navController = findNavController(this,  R.id.fragmentContainerView);
+        navbar.setupWithNavController(navController);
+
     }
 }
