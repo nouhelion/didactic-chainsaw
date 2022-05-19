ac0807013ffdc131b104c368c7201dc967f04d57
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-19 23:15:32 +0100

    bar deleted

--- application/app/src/main/java/com/careline/Home.java
+++ application/app/src/main/java/com/careline/Home.java
@@ -1,13 +1,8 @@
 package com.careline;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
 
@@ -15,9 +10,5 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        BottomNavigationView navbar  = findViewById(R.id.bottomNavigationView);
-        NavController navController = findNavController(this,  R.id.fragmentContainerView);
-        navbar.setupWithNavController(navController);
-
     }
 }
