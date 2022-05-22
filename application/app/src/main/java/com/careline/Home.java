924f58bf4b69c60619447e78b48131cb2dc9680e
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-22 02:12:21 +0100

    yaaaaaaaaaaaaaaay done services

--- application/app/src/main/java/com/careline/Home.java
+++ application/app/src/main/java/com/careline/Home.java
@@ -2,13 +2,81 @@ package com.careline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
+
+    ImageView psyim;
+    ImageView gynim;
+    ImageView dermim;
+    ImageView neurim;
+    ImageView cardim;
+    ImageView dentim;
+    ImageView pediim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        psyim=(ImageView) findViewById(R.id.psyim);
+       gynim=(ImageView) findViewById(R.id.gynim);
+        dermim=(ImageView) findViewById(R.id.dermim);
+        neurim=(ImageView) findViewById(R.id.neurim);
+        cardim=(ImageView) findViewById(R.id.cardim);
+        dentim=(ImageView) findViewById(R.id.dentim);
+        pediim=(ImageView) findViewById(R.id.pediim);
+        psyim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,rendez_vous.class);
+                startActivity(intent);
+            }
+        });
+        gynim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,rendez_vous.class);
+                startActivity(intent);
+            }
+        });
+        dermim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,rendez_vous.class);
+                startActivity(intent);
+            }
+        });
+        neurim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,rendez_vous.class);
+                startActivity(intent);
+            }
+        });
+        cardim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,rendez_vous.class);
+                startActivity(intent);
+            }
+        });
+        dentim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,rendez_vous.class);
+                startActivity(intent);
+            }
+        });
+        pediim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Home.this,rendez_vous.class);
+                startActivity(intent);
+            }
+        });
     }
 }
