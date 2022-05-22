08d28e5f9e7462a61b289c8ae0c802ea4d2b6df3
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-22 12:04:57 +0100

    yaaaaaaaaaaaaaaay done services

--- application/app/src/main/java/com/careline/informations.java
+++ application/app/src/main/java/com/careline/informations.java
@@ -2,13 +2,24 @@ package com.careline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class informations extends AppCompatActivity {
-
+    ImageView leave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informations);
+       leave=(ImageView) findViewById(R.id.shape);
+        leave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(informations.this,profil.class);
+                startActivity(intent);
+            }
+        });
     }
 }
