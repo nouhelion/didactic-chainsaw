34de3a84333491b8bf3dca701b82867ce0c76cdd
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-22 12:42:10 +0100

    update

--- application/app/src/main/java/com/careline/informations.java
+++ application/app/src/main/java/com/careline/informations.java
@@ -9,6 +9,9 @@ import android.widget.ImageView;
 
 public class informations extends AppCompatActivity {
     ImageView leave;
+    ImageView calendar;
+    ImageView notification;
+    ImageView home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,28 @@ public class informations extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(informations.this,profil.class);
+                startActivity(intent);
+            }
+        });
+        home=(ImageView) findViewById(R.id.hom);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(informations.this,Home.class);
+                startActivity(intent);
+            }
+        });
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(informations.this,notification.class);
+                startActivity(intent);
+            }
+        });
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(informations.this,rendez_vous.class);
                 startActivity(intent);
             }
         });
---
+++ application/app/src/main/java/com/careline/reservation.java
@@ -0,0 +1,14 @@
+package com.careline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+    }
+}
