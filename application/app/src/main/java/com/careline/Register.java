c7426557471a53c1c4444e58fad9513a1222fc72
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-22 23:23:16 +0100

    firebase and register

--- application/app/src/main/java/com/careline/Register.java
+++ application/app/src/main/java/com/careline/Register.java
@@ -1,14 +1,32 @@
 package com.careline;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Register extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth mAuth;
+   // ...
+  // Initialize Firebase Auth
+   private Button registerUser;
+   private EditText editTextfullname,editTextemail,editTextphone,editTextpassword;
+
 
     TextView login;
     Button welcome;
@@ -25,13 +43,96 @@ public class Register extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        welcome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(Register.this,Welcome.class);
-                startActivity(intent);
-            }
-        });
 
+
+
+        ///firebase stuff
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        registerUser=(Button) findViewById(R.id.insc);
+        registerUser.setOnClickListener(this);
+
+        editTextfullname=(EditText) findViewById(R.id.input_1);
+        editTextemail=(EditText) findViewById(R.id.input_2);
+        editTextphone=(EditText) findViewById(R.id.input_3);
+        editTextpassword=(EditText) findViewById(R.id.input_4);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.insc:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser() {
+        String email=editTextemail.getText().toString().trim();
+        String password=editTextpassword.getText().toString().trim();
+        String name=editTextfullname.getText().toString().trim();
+        String phone=editTextphone.getText().toString().trim();
+
+        if(name.isEmpty()){
+            editTextfullname.setError("le nom complet est obligatoire");
+            editTextfullname.requestFocus();
+            return;
+        }
+        if(email.isEmpty()){
+            editTextemail.setError("l'email est obligatoire");
+            editTextemail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextemail.setError("entrez un email valid");
+            editTextemail.requestFocus();
+            return;
+        }
+        if(phone.isEmpty()){
+            editTextphone.setError("le telephone est obligatoire");
+            editTextphone.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextpassword.setError("le mot de passe est obligatoire");
+            editTextpassword.requestFocus();
+            return;
+        }
+        if(password.length()<6){
+            editTextpassword.setError("le mot de passe est invalide");
+            editTextpassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user= new User(name,email,phone);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(Register.this,"User has been added successfully",Toast.LENGTH_LONG).show();
+                                            //redirect to login
+                                                Intent intent=new Intent(Register.this,Login.class);
+                                                startActivity(intent);
+                                            }else{
+                                                Toast.makeText(Register.this,"failed to register! try again!",Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    });
+                        }else{
+                            Toast.makeText(Register.this,"failed to register! try again!",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
---
+++ application/app/src/main/java/com/careline/User.java
@@ -0,0 +1,15 @@
+package com.careline;
+
+public class User {
+    public String fullname,email,phone;
+
+    public User() {
+    }
+
+    public User(String fullname, String email, String phone) {
+        this.fullname = fullname;
+        this.email = email;
+        this.phone = phone;
+    }
+
+}
