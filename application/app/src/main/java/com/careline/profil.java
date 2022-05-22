ef8635f87ddaec2bc4074d20a814e78c7d07413b
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-22 03:27:57 +0100

    yaaaaaaaaaaaaaaay done services

--- application/app/src/main/java/com/careline/profil.java
+++ application/app/src/main/java/com/careline/profil.java
@@ -2,13 +2,42 @@ package com.careline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class profil extends AppCompatActivity {
-
+    ImageView calendar;
+    ImageView notification;
+    ImageView home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        calendar=(ImageView) findViewById(R.id.cal);
+        notification=(ImageView) findViewById(R.id.not);
+        home=(ImageView) findViewById(R.id.hom);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(profil.this,Home.class);
+                startActivity(intent);
+            }
+        });
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(profil.this,notification.class);
+                startActivity(intent);
+            }
+        });
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(profil.this,rendez_vous.class);
+                startActivity(intent);
+            }
+        });
     }
 }
