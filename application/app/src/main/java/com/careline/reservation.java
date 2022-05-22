2fc6ba2212fc360f26769977af50320006dbe395
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-22 13:46:36 +0100

    reservation

--- application/app/src/main/java/com/careline/reservation.java
+++ application/app/src/main/java/com/careline/reservation.java
@@ -2,13 +2,24 @@ package com.careline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class reservation extends AppCompatActivity {
-
+    ImageView leave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
+        leave=(ImageView) findViewById(R.id.shape);
+        leave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(reservation.this,rendez_vous.class);
+                startActivity(intent);
+            }
+        });
     }
 }
