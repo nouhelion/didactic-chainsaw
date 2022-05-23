5a280c22490564bed327968dede48cc5e1a2d4a1
nouhelion <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-05-23 01:37:03 +0100

    firebase and login

--- application/app/src/main/java/com/careline/Login.java
+++ application/app/src/main/java/com/careline/Login.java
@@ -1,23 +1,43 @@
 package com.careline;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-    TextView register;
-    Button welcome;
+public class Login extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView register;
+    private EditText editTextemail,editTextpassword;
+    private Button login;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         register=(TextView) findViewById(R.id.insc);
-        welcome=(Button) findViewById(R.id.login);
+        login=(Button)findViewById(R.id.login);
+        login.setOnClickListener(this);
+
+        editTextemail=(EditText) findViewById(R.id.input_1);
+        editTextpassword=(EditText) findViewById(R.id.input_2);
+
+        mAuth = FirebaseAuth.getInstance();
+
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,13 +45,55 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        welcome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(Login.this,Welcome.class);
-                startActivity(intent);
-            }
-        });
 
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.login:
+                userLogin();
+                break;
+        }
+    }
+
+    private void userLogin() {
+        String email=editTextemail.getText().toString().trim();
+        String password=editTextpassword.getText().toString().trim();
+        if(email.isEmpty()){
+            editTextemail.setError("l'email est obligatoire");
+            editTextemail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextemail.setError("entrez un email valid");
+            editTextemail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextpassword.setError("le mot de passe est obligatoire");
+            editTextpassword.requestFocus();
+            return;
+        }
+        if(password.length()<6){
+            editTextpassword.setError("le mot de passe est invalide");
+            editTextpassword.requestFocus();
+            return;
+        }
+       mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+           @Override
+           public void onComplete(@NonNull Task<AuthResult> task) {
+               if(task.isSuccessful()){
+                   //redirect to welcome
+                   Intent intent=new Intent(Login.this,Welcome.class);
+                   startActivity(intent);
+                }
+               else{
+                   Toast.makeText(Login.this,"failed to connect! try again!",Toast.LENGTH_LONG).show();
+               }
+           }
+       });
     }
 }
